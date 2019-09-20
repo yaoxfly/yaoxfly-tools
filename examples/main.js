ce8d7950f1d088f509b5b1ed282e4c7931ba5e33
yaoxfly <1315892088@qq.com>
2019-09-20 21:35:45 +0800

    1

--- examples/main.js
+++ examples/main.js
@@ -2,11 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import Tools from "../lib/Tools";
 Vue.config.productionTip = false;
-
+// Vue.prototype.$Tools = Tools;
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    const res = Tools.Test1().rsName();
+    console.log(res);
+  }
 }).$mount("#app");
