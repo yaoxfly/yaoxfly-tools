b4dc5b17c03a2ddf62f1772cb08925411e910041
yaoxfly <1315892088@qq.com>
2019-09-22 23:00:18 +0800

    1

--- examples/main.js
+++ examples/main.js
@@ -5,10 +5,14 @@ import store from "./store";
 import Yxtool from "../lib";
 Vue.config.productionTip = false;
 let assembly = ["about", "home"];
+Vue.use(Yxtool);
+
 Yxtool.Tools.KeepAlive({ router, assembly });
 new Vue({
   router,
   store,
   render: h => h(App),
-  created() {}
+  created() {
+    console.log(this.$YxTest.getTest());
+  }
 }).$mount("#app");
